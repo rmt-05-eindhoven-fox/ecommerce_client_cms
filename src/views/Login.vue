db8d8ae9695f6bda075750d4ca6368d645fcef77
dave prastya <dave.prastya@gmail.com>
2020-11-10 22:23:22 +0700

    Push to new Fork

---
+++ src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="shadow-lg rounded container bg-dark text-white mb-3 p-3 rounded" id="div-container">
+    <h1>WELCOME</h1>
+    <form class="text-left mb-3">
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+      <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style>
+  #div-container{
+    width: 50%;
+  }
+</style>
