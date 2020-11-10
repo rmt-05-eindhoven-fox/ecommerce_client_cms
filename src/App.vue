f48d222429dc89ac08f3ba60b797959c6c220f3b
yosiaelnino <yosia.elnino@gmail.com>
2020-11-11 02:51:28 +0700

    1. Project initialization 2. Install Vue CLI 3. Create Home and Products router 4. Create template layout for products

---
+++ src/App.vue
@@ -0,0 +1,153 @@
+<template>
+  <div id="app">
+    <div class="page-container">
+      <md-app>
+        <md-app-toolbar class="md-transparent" md-elevation="1">
+          <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+            <md-icon>menu</md-icon>
+          </md-button>
+          <h3 class="md-title">SKoutwear Admin</h3>
+        </md-app-toolbar>
+        <md-app-drawer :md-active.sync="menuVisible">
+          <md-list>
+            <div class="user-profile">
+              <md-avatar class="md-avatar-icon md-large">
+                <md-icon>account_circle</md-icon>
+              </md-avatar>
+              <div class="user-menu">
+                <md-menu md-size="medium" md-align-trigger>
+                  <md-button md-menu-trigger>Admin@mail.com</md-button>
+                  <md-menu-content>
+                    <md-menu-item>
+                      <md-button>Logout</md-button>
+                    </md-menu-item>
+                  </md-menu-content>
+                </md-menu>
+              </div>
+            </div>
+            <md-divider></md-divider>
+            <div id="nav">
+              <md-list-item>
+                <md-icon>leaderboard</md-icon>
+                <span class="md-list-item-text">
+                  <router-link to="/">Dashboard</router-link>
+                </span>
+              </md-list-item>
+
+              <md-list-item>
+                <md-icon>shopping_bag</md-icon>
+                <span class="md-list-item-text">
+                  <router-link to="/products">Products</router-link>
+                </span>
+              </md-list-item>
+
+              <md-list-item>
+                <md-icon>category</md-icon>
+                <span class="md-list-item-text">
+                  <router-link to="/about">Categories</router-link>
+                </span>
+              </md-list-item>
+
+              <md-list-item>
+                <md-icon>view_carousel</md-icon>
+                <span class="md-list-item-text">
+                  <router-link to="/about">Banners</router-link>
+                </span>
+              </md-list-item>
+            </div>
+          </md-list>
+        </md-app-drawer>
+        <md-app-content>
+          <router-view />
+          <div class="speed-add">
+            <md-speed-dial class="md-bottom-right">
+              <md-speed-dial-target>
+                <md-icon class="md-morph-initial">add</md-icon>
+                <md-icon class="md-morph-final">edit</md-icon>
+              </md-speed-dial-target>
+
+              <md-speed-dial-content>
+                <md-button class="md-icon-button">
+                  <md-icon>view_carousel</md-icon>
+                  <md-tooltip md-direction="left">Banner</md-tooltip>
+                </md-button>
+
+                <md-button class="md-icon-button">
+                  <md-icon>category</md-icon>
+                  <md-tooltip md-direction="left">Category</md-tooltip>
+                </md-button>
+
+                <md-button class="md-icon-button">
+                  <md-icon>shopping_bag</md-icon>
+                  <md-tooltip md-direction="left">Product</md-tooltip>
+                </md-button>
+              </md-speed-dial-content>
+            </md-speed-dial>
+          </div>
+        </md-app-content>
+      </md-app>
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Poppins", sans-serif;
+  height: 100vh;
+  width: 100vw;
+  overflow-x: hidden;
+  scroll-behavior: smooth;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  font-size: 1.2rem;
+}
+
+#nav {
+  margin-top: 16px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.md-app {
+  height: 100vh;
+  width: 100vw;
+  overflow-x: hidden;
+  border: 1px solid rgba(#000, .12);
+}
+
+.md-app-content {
+  background-color: #f5f5f5 !important;
+}
+
+.md-drawer {
+  width: 230px;
+  max-width: calc(100vw - 125px);
+}
+
+.user-profile {
+  margin-top: 8px;
+}
+
+.user-menu {
+  margin-bottom: 16px;
+}
+</style>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    menuVisible: false
+  })
+}
+</script>
---
+++ src/views/Product.vue
@@ -0,0 +1,299 @@
+<template>
+  <div class="products">
+    <div class="md-layout md-gutter md-alignment-top-center">
+      <div class="md-layout-item md-size-15 md-medium-size-10 md-small-hide md-xsmall-hide"></div>
+      <div class="md-layout-item md-size-20 md-medium-size-25 md-small-size-25 md-xsmall-hide" style="background-color: white">
+        <h2 class="title">Categories</h2>
+        <md-list>
+          <md-list-item>
+            <p class="md-list-item-text">Shoes</p>
+          </md-list-item>
+          <md-list-item>
+            <p class="md-list-item-text">Bag</p>
+          </md-list-item>
+          <md-list-item>
+            <p class="md-list-item-text">Shirt</p>
+          </md-list-item>
+        </md-list>
+      </div>
+      <div class="md-layout-item main-content">
+        <div class="md-layout md-gutter">
+          <div class="md-layout-item">
+            <h2 class="text-left">All Products <span class="badge badge-success">6</span></h2>
+          </div>
+          <div class="md-layout-item">
+            <div class="text-right">
+              <md-button class="md-raised">Add</md-button>
+            </div>
+          </div>
+        </div>
+        <div class="products-content scrollable">
+          <md-card md-with-hover>
+            <md-ripple>
+              <div class="md-layout md-gutter">
+                <div class="md-layout-item md-size-25">
+                  <md-card-media>
+                    <img src="https://c.static-nike.com/a/images/t_prod_ss/w_640,c_limit,q_auto,f_auto/a2xc2snk9pr1tnwl8j53/womens-air-jordan-i-twist-release-date.jpg" alt="Product">
+                  </md-card-media>
+                </div>
+                <div class="md-layout-item">
+                  <md-card-header>
+                    <div class="md-title">
+                      <p class="text-left">Nike Air Jordan</p>
+                    </div>
+                  </md-card-header>
+                  <md-card-content>
+                    <div class="md-layout md-gutter">
+                      <div class="md-layout-item">
+                        <div class="price text-left">
+                          <md-icon>local_atm</md-icon>
+                          <span> Rp.2.000.000,-</span>
+                        </div>
+                        <div class="stock text-left">
+                          <md-icon>local_offer</md-icon>
+                          <span> 20 item(s)</span>
+                        </div>
+                      </div>
+                      <div class="md-layout-item">
+                        <div class="text-left">
+                          <p>Categories</p>
+                          <md-chip>Shoes</md-chip>
+                          <md-chip>Nike</md-chip>
+                          <md-chip>Hypebeast</md-chip>
+                        </div>
+                      </div>
+                    </div>
+                  </md-card-content>
+                </div>
+              </div>
+            </md-ripple>
+          </md-card>
+          <md-card md-with-hover>
+            <md-ripple>
+              <div class="md-layout md-gutter md-alignment-center">
+                <div class="md-layout-item md-size-25">
+                  <md-card-media>
+                    <img src="https://stockx-360.imgix.net//adidas-Yeezy-Boost-350-V2-Black/Images/adidas-Yeezy-Boost-350-V2-Black/Lv2/img01.jpg?auto=format,compress&w=559&q=90&dpr=2&updated_at=1603481985">
+                  </md-card-media>
+                </div>
+                <div class="md-layout-item">
+                  <md-card-header>
+                    <div class="md-title">
+                      <p class="text-left">Adidas Yeezy Boost</p>
+                    </div>
+                  </md-card-header>
+                  <md-card-content>
+                    <div class="md-layout md-gutter md-alignment-center">
+                      <div class="md-layout-item">
+                        <div class="price text-left">
+                          <md-icon>local_atm</md-icon>
+                          <span> Rp.5.000.000,-</span>
+                        </div>
+                        <div class="stock text-left">
+                          <md-icon>local_offer</md-icon>
+                          <span> 10 item(s)</span>
+                        </div>
+                      </div>
+                      <div class="md-layout-item">
+                        <div class="text-left">
+                          <p>Categories</p>
+                          <md-chip>Shoes</md-chip>
+                          <md-chip>Adidas</md-chip>
+                          <md-chip>Hypebeast</md-chip>
+                        </div>
+                      </div>
+                    </div>
+                  </md-card-content>
+                </div>
+              </div>
+            </md-ripple>
+          </md-card>
+          <md-card md-with-hover>
+            <md-ripple>
+              <div class="md-layout md-gutter">
+                <div class="md-layout-item md-size-25">
+                  <md-card-media>
+                    <img src="https://c.static-nike.com/a/images/t_prod_ss/w_640,c_limit,q_auto,f_auto/a2xc2snk9pr1tnwl8j53/womens-air-jordan-i-twist-release-date.jpg" alt="Product">
+                  </md-card-media>
+                </div>
+                <div class="md-layout-item">
+                  <md-card-header>
+                    <div class="md-title">
+                      <p class="text-left">Nike Air Jordan</p>
+                    </div>
+                  </md-card-header>
+                  <md-card-content>
+                    <div class="md-layout md-gutter">
+                      <div class="md-layout-item">
+                        <div class="price text-left">
+                          <md-icon>local_atm</md-icon>
+                          <span> Rp.2.000.000,-</span>
+                        </div>
+                        <div class="stock text-left">
+                          <md-icon>local_offer</md-icon>
+                          <span> 20 item(s)</span>
+                        </div>
+                      </div>
+                      <div class="md-layout-item">
+                        <div class="text-left">
+                          <p>Categories</p>
+                          <md-chip>Shoes</md-chip>
+                          <md-chip>Nike</md-chip>
+                          <md-chip>Hypebeast</md-chip>
+                        </div>
+                      </div>
+                    </div>
+                  </md-card-content>
+                </div>
+              </div>
+            </md-ripple>
+          </md-card>
+          <md-card md-with-hover>
+            <md-ripple>
+              <div class="md-layout md-gutter md-alignment-center">
+                <div class="md-layout-item md-size-25">
+                  <md-card-media>
+                    <img src="https://stockx-360.imgix.net//adidas-Yeezy-Boost-350-V2-Black/Images/adidas-Yeezy-Boost-350-V2-Black/Lv2/img01.jpg?auto=format,compress&w=559&q=90&dpr=2&updated_at=1603481985">
+                  </md-card-media>
+                </div>
+                <div class="md-layout-item">
+                  <md-card-header>
+                    <div class="md-title">
+                      <p class="text-left">Adidas Yeezy Boost</p>
+                    </div>
+                  </md-card-header>
+                  <md-card-content>
+                    <div class="md-layout md-gutter md-alignment-center">
+                      <div class="md-layout-item">
+                        <div class="price text-left">
+                          <md-icon>local_atm</md-icon>
+                          <span> Rp.5.000.000,-</span>
+                        </div>
+                        <div class="stock text-left">
+                          <md-icon>local_offer</md-icon>
+                          <span> 10 item(s)</span>
+                        </div>
+                      </div>
+                      <div class="md-layout-item">
+                        <div class="text-left">
+                          <p>Categories</p>
+                          <md-chip>Shoes</md-chip>
+                          <md-chip>Adidas</md-chip>
+                          <md-chip>Hypebeast</md-chip>
+                        </div>
+                      </div>
+                    </div>
+                  </md-card-content>
+                </div>
+              </div>
+            </md-ripple>
+          </md-card>
+          <md-card md-with-hover>
+            <md-ripple>
+              <div class="md-layout md-gutter">
+                <div class="md-layout-item md-size-25">
+                  <md-card-media>
+                    <img src="https://c.static-nike.com/a/images/t_prod_ss/w_640,c_limit,q_auto,f_auto/a2xc2snk9pr1tnwl8j53/womens-air-jordan-i-twist-release-date.jpg" alt="Product">
+                  </md-card-media>
+                </div>
+                <div class="md-layout-item">
+                  <md-card-header>
+                    <div class="md-title">
+                      <p class="text-left">Nike Air Jordan</p>
+                    </div>
+                  </md-card-header>
+                  <md-card-content>
+                    <div class="md-layout md-gutter">
+                      <div class="md-layout-item">
+                        <div class="price text-left">
+                          <md-icon>local_atm</md-icon>
+                          <span> Rp.2.000.000,-</span>
+                        </div>
+                        <div class="stock text-left">
+                          <md-icon>local_offer</md-icon>
+                          <span> 20 item(s)</span>
+                        </div>
+                      </div>
+                      <div class="md-layout-item">
+                        <div class="text-left">
+                          <p>Categories</p>
+                          <md-chip>Shoes</md-chip>
+                          <md-chip>Nike</md-chip>
+                          <md-chip>Hypebeast</md-chip>
+                        </div>
+                      </div>
+                    </div>
+                  </md-card-content>
+                </div>
+              </div>
+            </md-ripple>
+          </md-card>
+          <md-card md-with-hover>
+            <md-ripple>
+              <div class="md-layout md-gutter md-alignment-center">
+                <div class="md-layout-item md-size-25">
+                  <md-card-media>
+                    <img src="https://stockx-360.imgix.net//adidas-Yeezy-Boost-350-V2-Black/Images/adidas-Yeezy-Boost-350-V2-Black/Lv2/img01.jpg?auto=format,compress&w=559&q=90&dpr=2&updated_at=1603481985">
+                  </md-card-media>
+                </div>
+                <div class="md-layout-item">
+                  <md-card-header>
+                    <div class="md-title">
+                      <p class="text-left">Adidas Yeezy Boost</p>
+                    </div>
+                  </md-card-header>
+                  <md-card-content>
+                    <div class="md-layout md-gutter md-alignment-center">
+                      <div class="md-layout-item">
+                        <div class="price text-left">
+                          <md-icon>local_atm</md-icon>
+                          <span> Rp.5.000.000,-</span>
+                        </div>
+                        <div class="stock text-left">
+                          <md-icon>local_offer</md-icon>
+                          <span> 10 item(s)</span>
+                        </div>
+                      </div>
+                      <div class="md-layout-item">
+                        <div class="text-left">
+                          <p>Categories</p>
+                          <md-chip>Shoes</md-chip>
+                          <md-chip>Adidas</md-chip>
+                          <md-chip>Hypebeast</md-chip>
+                        </div>
+                      </div>
+                    </div>
+                  </md-card-content>
+                </div>
+              </div>
+            </md-ripple>
+          </md-card>
+        </div>
+      </div>
+      <div class="md-layout-item md-size-15 md-medium-size-10 md-small-hide md-xsmall-hide"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+  .products-content {
+    margin-top: 16px;
+    max-height: 80vh;
+    overflow: auto;
+  }
+  .stock {
+    margin-top: 16px;
+  }
+  .title {
+    margin-top: 8px;
+  }
+
+  .main-content {
+    margin-top: 8px;
+  }
+</style>
