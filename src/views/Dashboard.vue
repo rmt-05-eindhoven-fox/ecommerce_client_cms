4ebfcc8406f1b2d2ab413c42717c5ba7f0788f7d
dickyandeyan <dickyundead@gmail.com>
2020-11-12 00:01:52 +0700

    remove sidebar component

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,35 @@
 <template>
   <section class="dashboard">
     <!-- SIDEBAR -->
-    <Sidebar />
+    <div class="sidebar-sticky pt-3 sb">
+    <div class="user-login">
+      <p>Welcome,<br /><strong>Dicky Andeyan</strong></p>
+      <small>Logged in as Admin</small>
+    </div>
+    <ul class="nav flex-column">
+      <p
+        class="font-weight-bold pl-2"
+        style="color: #333399; background-color: #ffffff"
+      >
+        Navigation
+      </p>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          Dashboard <span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          Add Products
+        </a>
+      </li>
+      <li class="nav-item mt-5">
+        <a class="nav-link" href="#">
+          Logout
+        </a>
+      </li>
+    </ul>
+  </div>
     <div class="main-dash">
       <!-- LIST PRODUCT -->
       <div class="page-title">
@@ -17,13 +45,11 @@
 </template>
 
 <script>
-import Sidebar from '@/components/Sidebar.vue'
 import ProductCard from '@/components/ProductCard.vue'
 import AddForm from '@/components/AddForm.vue'
 export default {
   name: 'Dashboard',
   components: {
-    Sidebar,
     ProductCard,
     AddForm
   }
