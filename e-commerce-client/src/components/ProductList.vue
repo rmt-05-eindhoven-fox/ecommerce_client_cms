d9ecbe06213f44ccfa55025a7fd5bf9090cf377c
Deo Mareza <deomareza@gmail.com>
2020-11-14 11:37:20 +0700

    moved product List to separate component from home

---
+++ e-commerce-client/src/components/ProductList.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="content">
+  <form>
+    <div class="form-group">
+      <input v-model="searchInput" type="text" class="form-control" placeholder="Filter by Name" aria-describedby="emailHelp">
+    </div>
+  </form>
+  <ul class="list-group list-group-flush">
+    <ProductCard
+      v-for="product in productsFilter"
+      :key="product.id"
+      :productDetail="product"
+    ></ProductCard>
+  </ul>
+
+</div>
+</template>
+
+<script>
+import ProductCard from '@/components/ProductCard.vue'
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'ProductList',
+  components: {
+    ProductCard
+  },
+  data () {
+    return {
+      // products: [],
+      searchInput: ''
+    }
+  },
+  computed: {
+    products () {
+      return this.$store.state.products
+    },
+    productsFilter () {
+      if (this.searchInput) {
+        return this.products.filter(el => el.name.toLowerCase().includes(this.searchInput.toLowerCase()))
+      } else {
+        return this.products
+      }
+    }
+  },
+  methods: {
+    ...mapActions({
+      storeFetchProducts: 'fetchProducts'
+    }),
+    fetchProducts () {
+      this.storeFetchProducts()
+        .then(({ data }) => {
+          this.$store.commit('setProducts', data)
+          // this.products = data
+        })
+        .catch(error => {
+          console.log(error.response.status)
+        })
+    }
+  },
+  created () {
+    this.fetchProducts()
+  }
+}
+</script>
+
+<style>
+
+</style>
--- e-commerce-client/src/views/Home.vue
+++ e-commerce-client/src/views/Home.vue
@@ -3,7 +3,9 @@
     <Navbar></Navbar>
     <!-- <router-view></router-view> -->
 
-    <div class="content">
+    <ProductList />
+
+    <!-- <div class="content">
       <form>
         <div class="form-group">
           <input v-model="searchInput" type="text" class="form-control" placeholder="Filter by Name" aria-describedby="emailHelp">
@@ -17,57 +19,20 @@
         ></ProductCard>
       </ul>
 
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import ProductList from '../components/ProductList'
 import Navbar from '../components/Navbar'
-import ProductCard from '@/components/ProductCard.vue'
-import { mapActions } from 'vuex'
 
 export default {
   name: 'Home',
   components: {
-    ProductCard,
-    Navbar
-  },
-  data () {
-    return {
-      // products: [],
-      searchInput: ''
-    }
-  },
-  computed: {
-    products () {
-      return this.$store.state.products
-    },
-    productsFilter () {
-      if (this.searchInput) {
-        return this.products.filter(el => el.name.toLowerCase().includes(this.searchInput.toLowerCase()))
-      } else {
-        return this.products
-      }
-    }
-  },
-  methods: {
-    ...mapActions({
-      storeFetchProducts: 'fetchProducts'
-    }),
-    fetchProducts () {
-      this.storeFetchProducts()
-        .then(({ data }) => {
-          this.$store.commit('setProducts', data)
-          // this.products = data
-        })
-        .catch(error => {
-          console.log(error.response.status)
-        })
-    }
-  },
-  created () {
-    this.fetchProducts()
+    Navbar,
+    ProductList
   }
 }
 </script>
